4563f95704b1b25da64ee000b60258fe1c72006f
yapalfredo <36459847+yapalfredo@users.noreply.github.com>
2018-02-13 18:48:59 -0500

    Update MathOperators.java
    added casting to division

--- February13/MathOperators.java
+++ February13/MathOperators.java
@@ -5,10 +5,10 @@ public class MathOperators {
         int x = 2;
         int y = 19;
 
-        System.out.println("X + Y = " + (x + y));
-        System.out.println("X / Y = " + (x / y));
+         System.out.println("X + Y = " + (x + y));
+        System.out.println("Y / X = " + ((float)y/x));
         System.out.println("X^2 + Y^2 = " + (Math.pow(x, 2) + Math.pow(y, 2)));
         System.out.println("Square Root of Y^3 * 5 = " + (Math.sqrt(Math.pow(y, 3) * 5)));
-        System.out.println("LOG base 10 y^3/x^3 = " + Math.log10((Math.pow(y, 3) / Math.pow(x, 3))));
+        System.out.println("LOG base 10 y^3/x^2 = " + Math.log10((Math.pow(y, 3) / Math.pow(x, 2))));
     }
 }

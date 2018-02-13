5ac467f3eff4fd0766d9b406c94b153b24e951f7
yapalfredo <36459847+yapalfredo@users.noreply.github.com>
2018-02-13 17:31:51 -0500

    Assignment #2
    Assignment2 -> using and displaying String variable type
    Assignment2p2 -> using different kinds of variable types and display the value

---
+++ Assignment2.java
@@ -0,0 +1,17 @@
+// The name of my Java class is Assignment2
+// The java file name is Assignment2.java
+/* The java class name is the same with the java file name
+    with the addition of file extension .java
+ */
+public class Assignment2 {
+    
+    // name of the method is main
+    public static void main(String[] args) {
+
+        System.out.println("Dear Professor, this is my first program!");
+        String personalMessage = "You're awesome!";
+        System.out.println(personalMessage);
+
+    }
+
+}
---
+++ Assignment2p2.java
@@ -0,0 +1,32 @@
+// The name of my Java class is Assignment2p2
+// The java file name is Assignment2p2.java
+/* The java class name is the same with the java file name
+    with the addition of file extension .java
+ */
+public class Assignment2p2 {          
+    
+    // name of the method is main
+    public static void main(String[] args) {
+
+        String objectName = "Moon";
+        char objectInitial = 'M';
+        boolean isItAPlanet = false;
+        byte orbitalPeriodInDays = 27;
+        short radiusInMiles = 1079;
+        int distanceToEarthInMiles = 238900;
+        long volumeInKilometerCube = 219680000000000L;
+        float moonMassInKilogram = 7346000000000000000000000F;
+        double gravityInMetersPerSecondSquare  = 1.622; 
+       
+        System.out.println("The variable objectName = " + objectName);
+        System.out.println("The variable objectInitial = " + objectInitial);
+        System.out.println("The variable isItAPlanet = " + isItAPlanet);
+        System.out.println("The variable orbitalPeriodInDays = " + orbitalPeriodInDays);
+        System.out.println("The variable radiusInMiles = " + radiusInMiles);
+        System.out.println("The variable distanceToEarthInMiles = " + distanceToEarthInMiles);
+        System.out.println("The variable volumeInKilometerCube = " + volumeInKilometerCube);
+        System.out.println("The variable moonMassInKilogram = " + moonMassInKilogram);
+        System.out.println("The variable gravityInMetersPerSecondSquare = " + gravityInMetersPerSecondSquare);
+        
+    }
+}
